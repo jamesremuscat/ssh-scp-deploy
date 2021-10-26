815636d5349486ce96382c4dc4067116ce346d77
James Muscat <jamesremuscat@gmail.com>
2021-10-26 23:08:27 +0100

    Trim whitespace.

--- with_key.sh
+++ with_key.sh
@@ -1,25 +1,25 @@
 echo "🔑 Adding ssh key..." &&
 eval $(ssh-agent -s) &&
-ssh-add <(echo "${INPUT_KEY}") && 
+ssh-add <(echo "${INPUT_KEY}") &&
 echo "🔐 Added ssh key";
 
 PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
-    { 
+    {
         echo "👌 Executing pre-upload script..." &&
         ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
         echo "✅ Executed pre-upload script"
-    } || { 
+    } || {
         echo "😢 Something went wrong during pre-upload script" && exit 1
     }
 fi
 
 {
     echo "🚚 Uploading via scp..." &&
-    scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" && 
+    scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
     echo "🙌 Uploaded via scp"
 } || {
-    echo "😢 Something went wrong during upload" && exit 1 
+    echo "😢 Something went wrong during upload" && exit 1
 }
 
 POST_UPLOAD=${INPUT_POST_UPLOAD}
--- with_pass.sh
+++ with_pass.sh
@@ -4,7 +4,7 @@ if [ ! -z "$PRE_UPLOAD" ]; then
         echo "👌 Executing pre-upload script..." &&
         sshpass -p ${PASSWORD} ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
         echo "✅ Executed pre-upload script"
-    } || { 
+    } || {
         echo "😢 Something went wrong during pre-upload script" && exit 1
     }
 fi
